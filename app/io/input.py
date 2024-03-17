3d4978c261020433d0930351064079e1ee91ca66
Oleg Zagryvyi <olegzagraf@gmail.com>
2024-03-17 06:53:03 +0200

    Empty functions

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+def input_text_from_console():
+    pass
+
+
+def read_from_file_builtin():
+    pass
+
+
+def read_from_file_pandas():
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,6 @@
+def output_text_to_console():
+    pass
+
+
+def write_to_file_builtin():
+    pass
